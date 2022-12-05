f70ef4fd526d51ae17b2f683a975fc20361e924d
Rakshit Jain <rjain@yugabyte.com>
2022-12-05 12:50:51 +0530

    minor

--- src/main/java/driverDemo.java
+++ src/main/java/driverDemo.java
@@ -17,15 +17,15 @@ public class driverDemo {
             System.out.println("Connected to the PostgreSQL server successfully.");
 
 
-            int count = 4;
+            int count = 100;
             PreparedStatement pstmt = conn.prepareStatement("select * from pkey_hash_point_lookup_tbl_128_par_1 where col_varchar_id_1='1aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';");
             org.postgresql.PGStatement pgstmt = pstmt.unwrap(org.postgresql.PGStatement.class);
-//            pgstmt.setPrepareThreshold(0);
+            pgstmt.setPrepareThreshold(1000);
             for (int i = 0; i < count; i++) {
                 ResultSet rs = pstmt.executeQuery();
                 boolean usingServerPrepare = pgstmt.isUseServerPrepare();
                 while (rs.next()) {
-                    System.out.println(rs.getString(1)+usingServerPrepare);
+                    System.out.println(usingServerPrepare);
                 }
                 rs.close();
             }
